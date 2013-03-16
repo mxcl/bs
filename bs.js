2770f736e3dc25cde8c80c9a7b5806e65dc19674
Max Howell <mxcl@me.com>
2013-03-16 13:52:23 -0400

    Improve escapes with a `bs.js` library
    I resisted this, but it makes BS more useful, so what can you do?

---
+++ bs.js
@@ -0,0 +1,26 @@
+(function() {
+    bs = {
+        x: function(y) {
+            if (y === null || y === undefined)
+                return '';
+            return encodeURIComponent(y).replace('%20', '+');
+        },
+        X: function(y) {
+            if (y === null || y === undefined)
+                return '';
+            return encodeURIComponent(y).replace(/[!'()]/g, escape).replace(/\*/g, '%2A');
+        },
+        n: function(y) {
+            if (y === null || y === undefined)
+                return '';
+            return y;
+        },
+        N: function(y) {
+            if (y instanceof Array && y.length <= 0)
+                return '';
+            if (y instanceof Object && Object.keys(y).length <= 0)
+                return '';
+            return y || '';
+        }
+    };
+})();
